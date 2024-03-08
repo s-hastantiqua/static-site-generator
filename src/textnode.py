b741ea648101a5ff386c09a19a1a69bb1281b669
s-hastantiqua <s.hastantiqua@icloud.com>
2024-03-08 21:09:25 +0600

    add gitignore, main.sh, main.py and textnode.py

---
+++ src/textnode.py
@@ -0,0 +1,16 @@
+class TextNode():
+    
+    def __init__(self, text, text_type, url=None) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, __value: object) -> bool:
+        return (
+            self.text == __value.text
+            and self.text_type == __value.text_type
+            and self.url == __value.url
+        )
+    
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
