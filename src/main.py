acbcaa67c31432eee569d640db92f966fb93e5f5
s-hastantiqua <s.hastantiqua@icloud.com>
2024-03-17 01:28:24 +0600

    add logic to copy files from static to public after deleting everything from public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from textnode import TextNode
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(script_dir)
+
+dir_path_static = f"{parent_dir}/static"
+dir_path_public = f"{parent_dir}/public"
 
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 if __name__ == "__main__":
